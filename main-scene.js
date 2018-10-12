d21328dc6bd2ef6de3806ad5a101b1593774c9b1
Ari Sweedler <ari@sweedler.com>
2018-10-11 19:07:47 -0700

    Reformatted Assignment_One_Scene. Ready to start

--- main-scene.js
+++ main-scene.js
@@ -1,26 +1,32 @@
-window.Cube = window.classes.Cube =
-class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
-  { constructor()
-      { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
+/* Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube. */
+window.Cube = window.classes.Cube = class Cube extends Shape
+{
+  constructor()
+  {
+    /* Name the values we'll define per each vertex.  They'll have positions and normals. */
+    super( "positions", "normals" );
 
-        // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
-        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
-                                          [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
-                                          [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
-        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
-        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
-        this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
-                                          [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
-                                          [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+    /* First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube. */
+    this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                      [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                      [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+    /* Supply vectors that point away from eace face of the cube.  They should match up with the points in the above
+     * list Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color
+     * it accordingly. */
+    this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                      [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                      [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
 
-                 // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
-                 // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
-                 // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
-        this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
-                          14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
-        // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
-      }
+    /* Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the
+     * combined data you get if you look up index "i" of both lists above -- a position and a normal vector, together.
+     * Now let's tell it how to connect vertex entries into triangles.  Every three indices in this list makes one
+     * triangle: */
+    this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13, 14, 13, 15, 14, 16, 17, 18, 17,
+        19, 18, 20, 21, 22, 21, 23, 22 );
+    /* It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more
+     * automatically. */
   }
+}
 
 window.Transforms_Sandbox = window.classes.Transforms_Sandbox = class Transforms_Sandbox extends Tutorial_Animation
 {
@@ -67,7 +73,7 @@ window.Transforms_Sandbox = window.classes.Transforms_Sandbox = class Transforms
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
         let balls_out = top_box_transform.times( Mat4.translation([5*i, 5*j, 0]));
-        this.shapes.box.draw( graphics_state, balls_out, this.plastic.override({ color: Color.of( j/10,i/10,0,1 ) }) ); 
+        this.shapes.box.draw( graphics_state, balls_out, this.plastic.override({ color: Color.of( j/10,i/10,0,1 ) }) );
       }
     }
     this.shapes.ball.draw( graphics_state, ball_transform, this.plastic.override({ color: blue }) );
@@ -102,60 +108,77 @@ window.Cube_Single_Strip = window.classes.Cube_Single_Strip = class Cube_Single_
 }
 
 
-window.Assignment_One_Scene = window.classes.Assignment_One_Scene =
-class Assignment_One_Scene extends Scene_Component
-  { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
-      { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
-        if( !context.globals.has_controls   )
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
+window.Assignment_One_Scene = window.classes.Assignment_One_Scene = class Assignment_One_Scene extends Scene_Component
+/* The scene begins by requesting the camera, shapes, and materials it will need. */
+{
+  /* First, include a secondary Scene that provides movement controls: */
+  constructor( context, control_box )
+  {
+    super( context, control_box );
+    if( !context.globals.has_controls   ) {
+      context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
+    }
 
-        const r = context.width/context.height;
-        context.globals.graphics_state.    camera_transform = Mat4.translation([ 5,-10,-30 ]);  // Locate the camera here (inverted matrix).
-        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+    const r = context.width/context.height;
+    /* Locate the camera here (inverted matrix). */
+    context.globals.graphics_state.camera_transform = Mat4.translation([ 5,-10,-30 ]);
+    context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
 
-        const shapes = { 'box': new Cube(),               // At the beginning of our program, load one of each of these shape
-                       'strip': new Cube_Single_Strip(),  // definitions onto the GPU.  NOTE:  Only do this ONCE per shape
-                     'outline': new Cube_Outline() }      // design.  Once you've told the GPU what the design of a cube is,
-        this.submit_shapes( context, shapes );            // it would be redundant to tell it again.  You should just re-use
-                                                          // the one called "box" more than once in display() to draw
-                                                          // multiple cubes.  Don't define more than one blueprint for the
-                                                          // same thing here.
+    /* At the beginning of our program, load one of each of these shape definitions onto the GPU.  NOTE:  Only do this
+     * ONCE per shape design.  Once you've told the GPU what the design of a cube is, it would be redundant to tell it
+     * again.  You should just re-use the one called "box" more than once in display() to draw multiple cubes.  Don't
+     * define more than one blueprint for the same thing here. */
+    const shapes = { 'box': new Cube(), 'strip': new Cube_Single_Strip(), 'outline': new Cube_Outline() }
+    this.submit_shapes( context, shapes );
 
-                                     // Make some Material objects available to you:
-        this.clay   = context.get_instance( Phong_Shader ).material( Color.of( .9,.5,.9, 1 ), { ambient: .4, diffusivity: .4 } );
-        this.white  = context.get_instance( Basic_Shader ).material();
-        this.plastic = this.clay.override({ specularity: .6 });
+    /* Make some Material objects available to you: */
+    this.clay   = context.get_instance( Phong_Shader ).material( Color.of( .9,.5,.9, 1 ), { ambient: .4, diffusivity: .4 } );
+    this.white  = context.get_instance( Basic_Shader ).material();
+    this.plastic = this.clay.override({ specularity: .6 });
 
-        this.lights = [ new Light( Vec.of( 0,5,5,1 ), Color.of( 1, .4, 1, 1 ), 100000 ) ];
+    this.lights = [ new Light( Vec.of( 0,5,5,1 ), Color.of( 1, .4, 1, 1 ), 100000 ) ];
 
-        this.set_colors();
-      }
-    set_colors() {
-          // TODO:  Create a class member variable to store your cube's colors.
-      }
-    make_control_panel()             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-      { this.key_triggered_button( "Change Colors", [ "c" ], this.set_colors );    // Add a button for controlling the scene.
-        this.key_triggered_button( "Outline",       [ "o" ], () => {
-
-          // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off
-          } );
-        this.key_triggered_button( "Sit still",     [ "m" ], () => {
-
-          // TODO:  Requirement 3d:  Set a flag here that will toggle your swaying motion on and off.
-          } );
-      }
-    draw_box( graphics_state, model_transform )
-      {
-        // TODO:  Helper function for requirement 3 (see hint).
-        //        This should make changes to the model_transform matrix, draw the next box, and return the newest model_transform.
-
-        return model_transform;
-      }
-    display( graphics_state )
-      { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
-
-        let model_transform = Mat4.identity();
-
-        // TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper.
-      }
+    this.set_colors();
   }
+
+  set_colors()
+  {
+    /* TODO:  Create a class member variable to store your cube's colors. */
+
+  }
+
+  /* Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements. */
+  make_control_panel()
+  {
+    /* Add a button for controlling the scene. */
+    this.key_triggered_button( "Change Colors", [ "c" ], this.set_colors );
+
+    /* TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off */
+    this.key_triggered_button( "Outline", [ "o" ], () => {
+    } );
+
+    /* TODO:  Requirement 3d:  Set a flag here that will toggle your swaying motion on and off. */
+    this.key_triggered_button( "Sit still",     [ "m" ], () => {
+
+    } );
+  }
+
+  draw_box( graphics_state, model_transform )
+  {
+    /* TODO:  Helper function for requirement 3 (see hint). This should make changes to the model_transform matrix, draw
+     * the next box, and return the newest model_transform. */
+
+    return model_transform;
+  }
+
+  display( graphics_state )
+  {
+    /* Use the lights stored in this.lights. */
+    graphics_state.lights = this.lights;
+
+    let model_transform = Mat4.identity();
+
+    /* TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper. */
+  }
+
+}
